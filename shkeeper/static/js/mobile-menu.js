35ad7b821fd0af218398e1a7304ef248290b9894
dima <sdv@inet.ua>
2022-10-21 16:58:43 +0300

    Save shkeeper UI state - wip

--- shkeeper/static/js/mobile-menu.js
+++ shkeeper/static/js/mobile-menu.js
@@ -2,10 +2,15 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
 
-  
+
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
+
+    localStorage.setItem('menu-is-open', mobileMenuRef.classList.contains("is-open"));
   });
 
-  
+  // if (localStorage.getItem('menu-is-open') === 'false') {
+  //   mobileMenuRef.classList.remove("is-open");
+  // }
+
 })();
