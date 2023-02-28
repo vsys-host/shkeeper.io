e25853c1899cfc0379afd6416cad159db1ab6491
dima <sdv@inet.ua>
2023-02-28 15:09:33 +0200

    Fix Decimal(0) representation as 0E-8

--- shkeeper/utils.py
+++ shkeeper/utils.py
@@ -2,5 +2,5 @@ from decimal import Decimal
 
 
 def format_decimal(d: Decimal, precision: int = 8) -> str:
-    f = str(round(d.normalize(), precision))
+    f = str(round(d, precision).normalize())
     return f.rstrip('0').rstrip('.') if '.' in f else f
