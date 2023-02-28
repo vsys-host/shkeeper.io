54d4e29c57ab473b6d744c1d45e98b5d3820ea84
dima <sdv@inet.ua>
2023-02-28 22:07:33 +0200

    Improve string representation of decimal numbers

--- shkeeper/utils.py
+++ shkeeper/utils.py
@@ -1,6 +1,9 @@
 from decimal import Decimal
 
+def remove_exponent(d: Decimal) -> Decimal:
+    return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
 
 def format_decimal(d: Decimal, precision: int = 8) -> str:
-    f = str(round(d, precision).normalize())
-    return f.rstrip('0').rstrip('.') if '.' in f else f
+    # f = str(round(d, precision).normalize())
+    # return f.rstrip('0').rstrip('.') if '.' in f else f
+    return f'{remove_exponent(d):,}'
