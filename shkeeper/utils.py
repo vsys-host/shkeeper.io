3aac03bd3724a650eeae006a458edf2a9f550a67
dima <sdv@inet.ua>
2023-03-01 13:42:47 +0200

    don't separate thousands by default in format_decimal()

--- shkeeper/utils.py
+++ shkeeper/utils.py
@@ -3,7 +3,9 @@ from decimal import Decimal
 def remove_exponent(d: Decimal) -> Decimal:
     return d.quantize(Decimal(1)) if d == d.to_integral() else d.normalize()
 
-def format_decimal(d: Decimal, precision: int = 8) -> str:
-    # f = str(round(d, precision).normalize())
-    # return f.rstrip('0').rstrip('.') if '.' in f else f
-    return f'{remove_exponent(d):,}'
+def format_decimal(d: Decimal, precision: int = 8, st: bool = False) -> str:
+    # separate thousands
+    if st:
+        return f'{remove_exponent(d):,}'
+    else:
+        return str(remove_exponent(d))
