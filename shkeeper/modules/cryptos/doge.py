600bc8f9ebfc08246c134faecb15051008755957
dima <sdv@inet.ua>
2023-04-28 11:51:02 +0300

    Disable wallet creation for DOGE - fixes #15

--- shkeeper/modules/cryptos/doge.py
+++ shkeeper/modules/cryptos/doge.py
@@ -2,6 +2,7 @@ from shkeeper.modules.classes.bitcoin_like_crypto import BitcoinLikeCrypto
 
 
 class doge(BitcoinLikeCrypto):
+    wallet_created = True
     def __init__(self):
         self.crypto="DOGE"
     def getname(self):
