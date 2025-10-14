a679a55edffa3b38b47f326d8f51704fb3ae072b
oleh starosvitskyi <oleg.starosvitskij@gmail.com>
2025-10-14 15:56:56 +0200

    add default on bts

--- shkeeper/modules/classes/crypto.py
+++ shkeeper/modules/classes/crypto.py
@@ -54,7 +54,7 @@ class Crypto(abc.ABC):
             "solana_pyusd",
 
         ]
-        default_on = ["ltc", "doge"]
+        default_on = ["ltc", "btc", "doge"]
         for symbol in default_off:
             if cls.__name__ == symbol and (
                 f"{symbol.upper()}_WALLET" not in os.environ
