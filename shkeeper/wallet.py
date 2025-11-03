8c3f7c8bd71107b192dff7811cd5e5b949d72503
oleh starosvitskyi <oleg.starosvitskij@gmail.com>
2025-11-03 12:08:07 +0100

    add btc interface

--- shkeeper/wallet.py
+++ shkeeper/wallet.py
@@ -98,8 +98,11 @@ def payout(crypto_name):
     if isinstance(crypto, Ethereum) and crypto_name != "ETH":
         tmpl = "wallet/payout_eth.j2"
 
-    if crypto_name in ["ETH", "BNB", "BTC", "XRP", "MATIC", "AVAX", "SOL"]:
-        tmpl = "wallet/payout_coin.j2"
+    if crypto_name in ["ETH", "BNB", "XRP", "MATIC", "AVAX", "SOL"]:
+        tmpl = "wallet/payout_eth_coin.j2"
+
+    if crypto_name in ["BTC"]:
+        tmpl = "wallet/payout_btc_coin.j2"
 
     if "BTC-LIGHTNING" == crypto_name:
         tmpl = "wallet/payout_btc_lightning.j2"
