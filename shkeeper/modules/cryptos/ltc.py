d47a3700a806cb5f385a4085634e460e2c908434
Dmytro Samoylenko <sdv@inet.ua>
2026-02-20 10:17:29 +0000

    Change LTC name back to Litecoin

--- shkeeper/modules/cryptos/ltc.py
+++ shkeeper/modules/cryptos/ltc.py
@@ -1,8 +1,9 @@
 from shkeeper.modules.classes.ltc import Ltc
 
+
 class ltc(Ltc):
     def __init__(self):
         self.crypto = "LTC"
 
     def getname(self):
-        return "LTC"
+        return "Litecoin"
